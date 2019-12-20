53a42e891fe1b1f6fe9a4459f5b5b33d59aa4dc1
Tkaixiang <Tkaixiang>
2019-12-21 01:20:11 +0800

    Added Shards UI theme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  FormInput,
+  Collapse
+} from "shards-react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar></NavBar>
       </header>
     </div>
   );
+}
+
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+
+    this.state = {
+      dropdownOpen: false,
+      collapseOpen: false
+    };
+  }
+
+  toggleDropdown() {
+    this.setState({
+      ...this.state,
+      ...{
+        dropdownOpen: !this.state.dropdownOpen
+      }
+    });
+  }
+
+  toggleNavbar() {
+    this.setState({
+      ...this.state,
+      ...{
+        collapseOpen: !this.state.collapseOpen
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Navbar type="dark" theme="primary" expand="md">
+        <NavbarBrand href="#">Shards React</NavbarBrand>
+        <NavbarToggler onClick={this.toggleNavbar} />
+
+        <Collapse open={this.state.collapseOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink active href="#">
+                Active
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#" disabled>
+                Disabled
+              </NavLink>
+            </NavItem>
+            <Dropdown
+              open={this.state.dropdownOpen}
+              toggle={this.toggleDropdown}
+            >
+              <DropdownToggle nav caret>
+                Dropdown
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem>Action</DropdownItem>
+                <DropdownItem>Another action</DropdownItem>
+                <DropdownItem>Something else here</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Nav>
+
+          <Nav navbar className="ml-auto">
+            <InputGroup size="sm" seamless>
+              <InputGroupAddon type="prepend">
+                <InputGroupText>
+                  <FontAwesomeIcon icon={faSearch} />
+                </InputGroupText>
+              </InputGroupAddon>
+              <FormInput className="border-0" placeholder="Search..." />
+            </InputGroup>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
 }
 
 export default App;
