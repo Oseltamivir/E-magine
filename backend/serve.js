c1876a08517e22e0db9b9a071513c433909d0761
willi123yao <willi123yao@gmail.com>
2020-01-10 16:32:58 +0800

    Activate usage of v1 API in the web server

--- backend/serve.js
+++ backend/serve.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const apiv1 = require('./api/v1');
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -8,5 +10,7 @@ app.use((req, res, next) => {
   console.log(`${req.method} - ${req.url}`);
   next();
 });
+
+app.use('/api/v1', apiv1);
 
 app.listen(8080, () => console.log('Backend server is ready'));
