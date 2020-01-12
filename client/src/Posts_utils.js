fc6ecd99eccf8ddd5e19133e6bc9f52ff7b167ed
Warren <warrenlowkh@gmail.com>
2020-01-12 13:39:44 +0800

    Got the text editor working

--- client/src/Posts_utils.js
+++ client/src/Posts_utils.js
@@ -11,16 +11,30 @@ class Post extends Component{
                       PostTitle: '' ,
                       fileName: '',
                     postState:'Post!',
+                    post: ''
+
 
                   } //For storing text for post
+        this.editorOpen = true;
     }
     changeText = (ev) =>{let val = ev.target.value;let nam =ev.target.name;this.setState({[nam]: val})}
     postOrEdit = () =>{this.state.postState === 'Post!'? this.setState({postState: "Edit!"}):this.setState({postState: "Post!"})}
     componentDidMount(){
-      var editor = new window.FroalaEditor('.exampl')
+      var self = this;
+      this.editor = new window.FroalaEditor('.exampl', {
+        events: {
+          contentChanged: function() {
+            self.setState({post: this.html.get()});
+          }
+        },
+        attribution: false
+      })
     }
     componentDidUpdate(){
-      var editor = new window.FroalaEditor('.exampl')
+      // create a variable to check if the thingy is open
+      // if it is not and user is poster, create editor
+      if (this.editorOpen 
+      if (this.props.user === this.props.poster) this.editor = new window.FroalaEditor('.exampl')
     }
     render(){
         let ifPosted = ''
@@ -36,9 +50,10 @@ class Post extends Component{
         return(
           <div id = 'postall'>
         <form id = 'post' onSubmit = {this.func = (ev) => {ev.preventDefault();alert('Posted!')}}>
-        <div class = 'exampl'>
+        <div className = 'exampl'>
       </div>   
 <Button id = 'spaceOut' type = 'primary' onClick = {this.postOrEdit}>{this.state.postState}</Button>
+        <div dangerouslySetInnerHTML={{__html: this.state.post}}></div> {/* might want to remove later */}
 </form>
 {ifPosted}
 </div>
