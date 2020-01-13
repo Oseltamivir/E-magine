2eb0692015c48d4a6ee2b47b225f7101719c9169
Warren <warrenlowkh@gmail.com>
2020-01-13 13:30:24 +0800

    Put Topics in Explore Page

--- client/src/Explore.js
+++ client/src/Explore.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Card, message, Spin, List } from 'antd';
 import './index.css';
-
 import InfiniteScroll from 'react-infinite-scroller';
-
+import DiscApp from './DiscApp'
+import { Link, Switch, Route, Redirect } from 'react-router-dom';
 const { Meta } = Card;
+
 
 
 class Explore extends React.Component {
@@ -16,9 +17,11 @@ class Explore extends React.Component {
             data: [],
             hasMore: true,
             loading: false,
+            container:{color: 'red', fontSize:'244%'}
+            }
         };
-    }
 
+    
     cardClick() {
         alert("Live chat in development...")
     }
@@ -69,9 +72,32 @@ class Explore extends React.Component {
                 msOverflowStyle: "none",
             }}>
                 <h1>Explore Page in Development!</h1>
+                <div>
+            <Link to='/DiscApp'>
+            <h1 style = {this.state.container}>Hello(LINK TO DISCUSSION PAGE FOR DEBUGGING PURPOSES)</h1>
+            </Link> 
+            <Link to ={{ pathname: '/Topicpage',
+                        state:{topicName:'Computing'}
+                }}>
+            <h1 style = {this.state.container}>Computing</h1>
+            </Link>
+            <h1 style = {this.state.container}>Maths</h1>
+            <h1 style = {this.state.container}>Chemistry</h1>
+            <h1 style = {this.state.container}>Physics</h1>
+            <h1 style = {this.state.container}>General Paper</h1>
+            <h1 style = {this.state.container}>Biology</h1>
+            <h1 style = {this.state.container}>Geography</h1>
+            <h1 style = {this.state.container}>History</h1>
+            <h1 style = {this.state.container}>CLB</h1>
+            <h1 style = {this.state.container}>CSC</h1>
+        </div>  
             </div>
+            
         );
     }
 }
 
 export default Explore;
+
+
+
