9d62a1ab5f384c88c33dd0019c2b8d25836dc821
Warren <warrenlowkh@gmail.com>
2020-01-14 23:51:35 +0800

    Added to-do-list variant for Streams

---
+++ client/src/Streamlists.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './DiscApp.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import { Button, Input, Icon } from 'antd'
+import Listmaker from './Listmaker.js'
+import { getElementError } from '@testing-library/react';
+const { TextArea } = Input
+class Streamlist extends Component {
+    constructor(prop) {
+        super(prop)
+        this.state = {
+            items: [],
+            currentState: { key: '', text: '', user: '', counter: '' },
+            currentText: '',
+        }
+    }
+    deleteItem = (key) => { const filteredItems = this.state.items.filter(item => { return item.key !== key }); this.setState({ items: filteredItems }) }
+    addItem = (ev) => { ev.preventDefault(); let newState = { key: Date.now() + this.props.user, text: this.state.currentText, user: this.props.user, counter: 0 }; if (newState.text !== '') { let item = [...this.state.items, newState]; this.setState({ currentState: newState, items: item, currentText: '' }) } else { alert('Wrong Input') } }
+    handleItem = (ev) => { this.setState({ currentText: ev.target.value }) }
+    render() {
+        return (
+            <div className='todolist' >
+                <form onSubmit={this.addItem}>
+                    <Listmaker
+                        entries={this.state.items}
+                        deleteItem={this.deleteItem.bind(this)}
+                        user={this.props.user}
+                    />
+                    <div id='Postbar'>
+                        <TextArea id='Messaging' placeholder='Type something here' value={this.state.currentText} onChange={this.handleItem}></TextArea>
+                        <Button id='posttwo' size='large' type='primary' onClick={this.addItem}>Post</Button>
+                        <Button id='postthree' size='large' type='primary' onClick={this.addAnswer}>
+                            <Icon type="star" theme="twoTone" />Add Answer
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default Streamlist
--- client/src/StreamsDiscussion.js
+++ client/src/StreamsDiscussion.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './DiscApp.css';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import { Button, Badge,Carousel } from 'antd'
+import { Button, Badge, Carousel, Layout } from 'antd'
 import CarouselSlide from "./CauroselSlide"
+import Streamlist from './Streamlists'
 //This will be the discussion page mainframe
+const { Content, Sider } = Layout;
 class StreamDisc extends Component {
     constructor() {
         super()
         this.state = {
             user: 'YEET6',
-            category: 'Math', // For storing category
             notifications: 9999, //Stores number of notifications  
             poster: 'YEET6',
             items: {},
@@ -19,24 +20,27 @@ class StreamDisc extends Component {
     }
     render() {
         return (
-            <div id='All'>
+            <Layout id='All'>
                 <div id='header'>
                     <h1 id='Category'>{this.state.category}
                     </h1>
                     <br />
                 </div>{/*Div for id 'Header */}
-                <div id="showcase" style={{ marginLeft: "1.3vw", marginBottom: "10vh" }}>
-                    <Carousel dotPosition="">
-                        <div>
-                            <CarouselSlide username="ThatHentaiUser" url="https://www.youtube.com/watch?v=VF3M6l9rsqs" viewers={156} desc="Waa! Waa! Jugemu Jugemu Go-Kō-no-Surikire Kaijari-suigyo no Suigyō-matsu Unrai-matsu Fūrai-matsu Kū-Neru Tokoro ni Sumu Tokoro Yaburakōji no Burakōji Paipo Paipo Paipo no Shūringan Shūringan no Gūrindai Gūrindai no Ponpokopii no Ponpokonā no Chōkyūmei no Chōsuke hit me and gave me a lump on my head What? Our Jugemu Jugemu Go-Kō-no-Surikire Kaijari-suigyo no Suigyō-matsu Unrai-matsu Fūrai-matsu Kū-Neru Tokoro ni Sumu Tokoro Yaburakōji no Burakōji Paipo Paipo Paipo no Shūringan Shūringan no Gūrindai Gūrindai no Ponpokopii no Ponpokonā no Chōkyūmei no Chōsuke hit you and gave you a lump on your head? I'm so sorry! Honey, did you hear that? It seems that Jugemu Jugemu Go-Kō-no-Surikire Kaijari-suigyo no Suigyō-matsu Unrai-matsu Fūrai-matsu Kū-Neru Tokoro ni Sumu Tokoro Yaburakōji no Burakōji Paipo Paipo Paipo no Shūringan Shūringan no Gūrindai Gūrindai no Ponpokopii no Ponpokonā no Chōkyūmei no Chōsuke hit Kin-chan here, and gave him a lump on his head!
-Really? Our Jugemu Jugemu Go-Kō-no-Surikire Kaijari-suigyo no Suigyō-matsu Unrai-matsu Fūrai-matsu Kū-Neru Tokoro ni Sumu Tokoro Yaburakōji no Burakōji Paipo Paipo Paipo no Shūringan Shūringan no Gūrindai Gūrindai no Ponpokopii no Ponpokonā no Chōkyūmei no Chōsuke did that? We'd better call Jugemu Jugemu Go-Kō-no-Surikire Kaijari-suigyo no Suigyō-matsu Unrai-matsu Fūrai-matsu Kū-Neru Tokoro ni Sumu Tokoro Yaburakōji no Burakōji Paipo Paipo Paipo no Shūringan Shūringan no Gūrindai Gūrindai no Ponpokopii no Ponpokonā no Chōkyūmei no Chōsuke in here and sort this out. Can I see your lump, Kin-chan?
-It took so long to explain, the lump's already gone down!"/>
-                        </div>
-                    </Carousel>
-                </div>
-                <br />
+                <Content>
+                    <div id="showcase" style={{ marginLeft: "1.3vw", marginBottom: "10vh" }}>
+                        <Carousel dotPosition="">
+                            <div>
+                                <CarouselSlide username="ThatCapitalistGopnik" url="https://www.youtube.com/watch?v=2SLvtP6KMUM&gl=SG" viewers={156} desc="State Anthem of the Soviet UnionAlexandrow-Ensemble Союз нерушимый республик свободных Сплотила навеки Великая Русь. Да здравствует созданный волей народов Единый, могучий Советский Союз! Славься, Отечество наше свободное, Дружбы, народов надежный оплот! Знамя советское, знамя народное Пусть от победы, к победе ведет! Сквозь грозы сияло нам солнце свободы, И Ленин великий нам путь озарил. Нас вырастил Сталин - на верность народу На труд и на подвиги нас вдохновил. Славься, Отечество чаше свободное, Счастья народов надежный оплот! Знамя советское, знамя народное Пусть от победы к победе ведет! Skvoz grozy siialo nam solntse svobody, I Lenin velikij nam put ozaril. Nas vyrastil Stalin - na vernost narodu Na trud i na podvigi nas vdokhnovil. Slavsia, Otechestvo chashe svobodnoe, Schastia narodov nadezhnyj oplot! Znamia sovetskoe, znamia narodnoe Pust ot pobedy k pobede vedet! Мы армию нашу растили в сраженьях, Захватчиков подлых с дороги сметем! Мы в битвах решаем судьбу поколений, Мы к славе Отчизну свою поведем! Славься, Отечество наше свободное, Славы народов надежный оплот! Знамя советское, знамя народное Пусть от победы к победе ведет!"
+                                />
+                            </div>
+                        </Carousel>
+                    </div>
+                </Content>
+                <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} width="15vw" style={{ boxShadow: "3px 0px 10px" }}>
+                    <Streamlist />
+                </Sider>
                 {/*div for 'all*/}
-            </div>
+            </Layout>
         )
     }
 }
