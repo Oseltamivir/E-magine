5befa1ebe960edc5cae9c2ea47e259f280474f7d
Warren <warrenlowkh@gmail.com>
2020-01-14 23:34:04 +0800

    Took out redundant files, added needed dependencies

--- client/src/StreamsDiscussion.js
+++ client/src/StreamsDiscussion.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './DiscApp.css';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import Post from './Posts_utils.js'
-import Todo from './to-do-list.js'
-import { Button, Badge } from 'antd'
-import Notes from './notifications.js'
+import { Button, Badge,Carousel } from 'antd'
+import CarouselSlide from "./CauroselSlide"
 //This will be the discussion page mainframe
 class StreamDisc extends Component {
     constructor() {
@@ -28,7 +26,7 @@ class StreamDisc extends Component {
                     <br />
                 </div>{/*Div for id 'Header */}
                 <div id="showcase" style={{ marginLeft: "1.3vw", marginBottom: "10vh" }}>
-                    <Carousel dotPosition="right">
+                    <Carousel dotPosition="">
                         <div>
                             <CarouselSlide username="ThatHentaiUser" url="https://www.youtube.com/watch?v=VF3M6l9rsqs" viewers={156} desc="Waa! Waa! Jugemu Jugemu Go-Kō-no-Surikire Kaijari-suigyo no Suigyō-matsu Unrai-matsu Fūrai-matsu Kū-Neru Tokoro ni Sumu Tokoro Yaburakōji no Burakōji Paipo Paipo Paipo no Shūringan Shūringan no Gūrindai Gūrindai no Ponpokopii no Ponpokonā no Chōkyūmei no Chōsuke hit me and gave me a lump on my head What? Our Jugemu Jugemu Go-Kō-no-Surikire Kaijari-suigyo no Suigyō-matsu Unrai-matsu Fūrai-matsu Kū-Neru Tokoro ni Sumu Tokoro Yaburakōji no Burakōji Paipo Paipo Paipo no Shūringan Shūringan no Gūrindai Gūrindai no Ponpokopii no Ponpokonā no Chōkyūmei no Chōsuke hit you and gave you a lump on your head? I'm so sorry! Honey, did you hear that? It seems that Jugemu Jugemu Go-Kō-no-Surikire Kaijari-suigyo no Suigyō-matsu Unrai-matsu Fūrai-matsu Kū-Neru Tokoro ni Sumu Tokoro Yaburakōji no Burakōji Paipo Paipo Paipo no Shūringan Shūringan no Gūrindai Gūrindai no Ponpokopii no Ponpokonā no Chōkyūmei no Chōsuke hit Kin-chan here, and gave him a lump on his head!
 Really? Our Jugemu Jugemu Go-Kō-no-Surikire Kaijari-suigyo no Suigyō-matsu Unrai-matsu Fūrai-matsu Kū-Neru Tokoro ni Sumu Tokoro Yaburakōji no Burakōji Paipo Paipo Paipo no Shūringan Shūringan no Gūrindai Gūrindai no Ponpokopii no Ponpokonā no Chōkyūmei no Chōsuke did that? We'd better call Jugemu Jugemu Go-Kō-no-Surikire Kaijari-suigyo no Suigyō-matsu Unrai-matsu Fūrai-matsu Kū-Neru Tokoro ni Sumu Tokoro Yaburakōji no Burakōji Paipo Paipo Paipo no Shūringan Shūringan no Gūrindai Gūrindai no Ponpokopii no Ponpokonā no Chōkyūmei no Chōsuke in here and sort this out. Can I see your lump, Kin-chan?
