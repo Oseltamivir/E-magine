ddef1e0e2b63c5a8d87de04b2aebae42e37b84cf
Warren <warrenlowkh@gmail.com>
2020-01-15 18:04:44 +0800

    Created DM messaging files

---
+++ client/src/DirectMessagesMessages.js
@@ -0,0 +1,50 @@
+// Makes a list for js
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './DiscApp.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import Post from './Posts_utils.js'
+import { Button, Divider, Icon } from 'antd'
+
+
+export default class DirectMessagesMessages extends Component {
+    constructor(prop) {
+        super(prop)
+    }
+
+    postTime = (items) => {
+        let d = new Date()
+        return items.user + ' posted this at ' + d.toDateString()
+
+    }
+    createItem = (items) => {
+        return (
+            <li key={items.key}>
+                <div>
+                    <p className='timetext'>{this.postTime(items)}</p>
+                    <p className='replytext'>{items.text}</p>
+                </div>
+                <span>
+                    <Button type={'danger'} onClick={() => { this.props.deleteItem(items.key) }}>Delete
+                    </Button>
+                </span>
+                <br />
+            </li>
+        );
+    }
+
+    render() {
+        const toDoEntries = this.props.entries;
+        const listItems = toDoEntries.map(this.createItem);
+        return (
+            <div id='listdesign'>
+                <Divider orientation="left" style={{ color: "white", fontSize: "2vw" }}>
+                    <span>Your chat with {this.props.receiver}
+                        <Icon type="edit" theme="twoTone" />
+                    </span>
+                </Divider>
+                <ul id='replylist'>{listItems}</ul>
+            </div>
+        )
+    }
+}
