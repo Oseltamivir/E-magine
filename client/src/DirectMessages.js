f559527c071fa1eb86e767888119ec7422282255
Warren <warrenlowkh@gmail.com>
2020-01-16 01:49:36 +0800

    Made state for receiver, sender and pass as props

--- client/src/DirectMessages.js
+++ client/src/DirectMessages.js
@@ -1,43 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './DiscApp.css';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import Post from './Posts_utils.js'
 import Todo from './to-do-list.js'
-import {Button, Badge} from 'antd'
-//This will be the discussion page mainframe
-class DirectMsg extends Component{
-  constructor(){
+import { Button, Badge } from 'antd'
+import DirectMessagesPostbar from './DirectMessagesPostbar'
+//This will be the direct messages mainframe
+class DirectMsg extends Component {
+  constructor() {
     super()
     this.state = {
-                  user: 'YEET6',
-                  category: 'Math', // For storing category
-                  notifications:9999 , //Stores number of notifications  
-                  poster: 'YEET6',
-                  items : {},                                
-                                  }
-              
-  }
-  render(){
-    return(
-      <div id = 'All'>
-      <div id = 'header'>
-      <h1 id = 'Category'>{this.state.category}
-      <span id = 'Status'>
-      <Button type = 'primary' size = 'small' id = 'pfp' onClick = {this.changeUser = () =>{
-        this.state.user === 'user'? this.setState({user:'YEET6'}):this.setState({user:'user'})
-      ;alert('User Changed')
-      }}></Button>   
-      </span>
-      </h1>
-      <br/>
-      </div>{/*Div for id 'Header */}
-      <DirectMessagePostbar/>
-      {/*div for 'all*/}
-    </div> 
-   )
+      receiver: 'Bob',
+      sender: 'Charlie',
+      notifications: 9999, //Stores number of notifications  
+      items: {},
     }
+
   }
+  render() {
+    return (
+      <div id='All'>
+        <div id='header'>
+          <h1 id='Category'>{this.state.receiver}
+            <span id='Status'>
+              <Button type='primary' size='small' id='pfp' onClick={this.changeUser = () => {
+                this.state.sender === 'Charlie' ? this.setState({ sender: 'YEET6' }) : this.setState({ sender: 'Charlie' })
+                  ; alert('User Changed')
+              }}></Button>
+            </span>
+          </h1>
+          <br />
+        </div>{/*Div for id 'Header */}
+        <DirectMessagesPostbar sender={this.state.sender} receiver={this.state.receiver} />
+        {/*div for 'all*/}
+      </div>
+    )
+  }
+}
 
 
 export default DirectMsg
