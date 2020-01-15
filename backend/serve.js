0311c674991a675ec7d16f0f2315d1eb15b7760c
willi123yao <willi123yao@gmail.com>
2020-01-15 21:04:47 +0800

    Better logging using prefix

--- backend/serve.js
+++ backend/serve.js
@@ -7,10 +7,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
-  console.log(`${req.method} - ${req.url}`);
+  console.log(`[LOG] ${req.method} - ${req.url}`);
   next();
 });
 
+console.log('[LOG] Loading v1 API');
 app.use('/api/v1', apiv1);
 
-app.listen(8080, () => console.log('Backend server is ready'));
+app.listen(8080, () => console.log('[LOG] Backend server is ready'));
