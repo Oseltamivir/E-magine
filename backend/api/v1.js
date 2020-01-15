bbc801e439f8ebf3213c44a29e233d6405de7600
willi123yao <willi123yao@gmail.com>
2020-01-15 21:12:48 +0800

    Update login route to use backend database
    Hash of password using SHA256

--- backend/api/v1.js
+++ backend/api/v1.js
@@ -5,6 +5,7 @@ const RD = require('reallydangerous');
 const signer = new RD.TimestampSigner('my-secret');
 
 const MongoClient = require('mongodb').MongoClient;
+const crypto = require('crypto');
 
 const env = require('../env.json');
 
@@ -22,18 +23,28 @@ router.get('/version', (req, res) => {
   res.json({version: 1});
 });
 
-router.post('/auth/login', (req, res) => {
+router.post('/auth/login', async (req, res) => {
+  if (!req.body.hasOwnProperty('username') || !req.body.hasOwnProperty('password')) {
+    res.status(400).json({success: false, error: 'Username and password is required!'});
+    return;
+  }
+
   const username = req.body.username;
   const password = req.body.password;
 
-  // TODO: Implement database backed authentication
-  if (username == 'tkaixiang' || username == 'willi123yao') {
-    const token = signer.sign(Buffer.from(username).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, ''));
-    res.json({success: true, token});
+  // Hash user password (in hex)
+  const hashed = crypto.createHash('sha256').update(password).digest('hex');
+
+  // Check login
+  const user = await db.collection('users').findOne({username, password: hashed});
+  if (!user) {
+    res.status(403).json({success: false, error: 'Invalid username or password!'});
+    return;
   }
-  else {
-    res.json({success: false, error: 'Invalid username or password!'});
-  }
+
+  // Generate API token for user
+  const token = signer.sign(Buffer.from(username).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, ''));
+  res.json({success: true, token});
 });
 
 module.exports = router;
