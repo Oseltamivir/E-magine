9b62c08dd04c40d78063e5aa9ef0c669d3844886
Warren <warrenlowkh@gmail.com>
2020-01-17 17:46:16 +0800

    Made file to show list of posts

---
+++ client/src/Streamlistmake.js
@@ -0,0 +1,48 @@
+// Makes a list for js
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './DiscApp.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import Post from './Posts_utils.js'
+import { Button, Divider, Icon } from 'antd'
+
+
+export default class Streamlistmake extends Component {
+    constructor(prop) {
+        super(prop)
+    }
+
+    postTime = (items) => {
+        let d = new Date()
+        return items.user + ' posted this at ' + d.toDateString()
+
+    }
+    createItem = (items) => {
+        return (
+            <li key={items.key}>
+                <div>
+                    <p className='timetext'>{this.postTime(items)}</p>
+                    <p className='replytext'>{items.text}</p>
+                </div>
+                <span>
+                    <Button type={'danger'} onClick={() => { this.props.deleteItem(items.key) }}>Delete
+                    </Button>
+                </span>
+                <br />
+            </li>
+        );
+    }
+    render() {
+        const toDoStreamItems = this.props.entries;
+        const streamListItems = toDoStreamItems.map(this.createItem);
+        return (
+            <div id='listdesign'>
+                <Divider orientation="left" style={{ color: "white", fontSize: "2vw" }}>
+                    <span>Chat Messages</span>
+                    <Icon type="edit" theme="twoTone" />
+                </Divider>
+                <ul id='replylist'>{streamListItems}</ul>
+            </div>
+        )
+    }
+}
