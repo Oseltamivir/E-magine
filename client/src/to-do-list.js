5bbe78dfa9bc71eab3b94c50c6e74ec9529c9066
Warren <warrenlowkh@gmail.com>
2020-01-18 01:03:34 +0800

    Improved readability of discussion page code

--- client/src/to-do-list.js
+++ client/src/to-do-list.js
@@ -20,38 +20,68 @@ class Todo extends Component {
     }
     upvoteAnswer = (key) => {
         let ind = this.state.answerItems.findIndex(x => x.key === key);
-        const newAnswer = { key: this.state.answerItems[ind].key, text: this.state.answerItems[ind].text, user: this.state.answerItems[ind].user, counter: this.state.answerItems[ind].counter + 1 }
+        const newAnswer = {
+            key: this.state.answerItems[ind].key,
+            text: this.state.answerItems[ind].text,
+            user: this.state.answerItems[ind].user,
+            counter: this.state.answerItems[ind].counter + 1
+        }
         let changedItem = this.state.answerItems
         changedItem[ind] = newAnswer
         this.setState({ answerItems: changedItem })
 
     }
-    upvoteReply = (key) => {
-        let ind = this.state.items.findIndex(x => x.key === key);
-        const newReply = { key: this.state.items[ind].key, text: this.state.items[ind].text, user: this.state.items[ind].user, counter: this.state.items[ind].counter + 1 }
-        let changedItem = this.state.items
-        changedItem[ind] = newReply
-        this.setState({ items: changedItem })
-    }
+
     downvoteAnswer = (key) => {
         let ind = this.state.answerItems.findIndex(x => x.key === key);
-        const newAnswer = { key: this.state.answerItems[ind].key, text: this.state.answerItems[ind].text, user: this.state.answerItems[ind].user, counter: this.state.answerItems[ind].counter - 1 }
+        const newAnswer = {
+            key: this.state.answerItems[ind].key,
+            text: this.state.answerItems[ind].text,
+            user: this.state.answerItems[ind].user,
+            counter: this.state.answerItems[ind].counter - 1
+        }
         let changedItem = this.state.answerItems
         changedItem[ind] = newAnswer
         this.setState({ answerItems: changedItem })
     }
-    downvoteReply = (key) => {
-        let ind = this.state.items.findIndex(x => x.key === key);
-        const newReply = { key: this.state.items[ind].key, text: this.state.items[ind].text, user: this.state.items[ind].user, counter: this.state.items[ind].counter - 1 }
-        let changedItem = this.state.items
-        changedItem[ind] = newReply
-        this.setState({ items: changedItem })
+    deleteItem = (key) => {
+        const filteredItems = this.state.items.filter(item => { return item.key !== key });
+        this.setState({ items: filteredItems })
     }
-    deleteItem = (key) => { const filteredItems = this.state.items.filter(item => { return item.key !== key }); this.setState({ items: filteredItems }) }
-    deleteAnswer = (key) => { const filteredAnswers = this.state.answerItems.filter(item => { return item.key !== key }); this.setState({ answerItems: filteredAnswers }) }
-    addItem = (ev) => { ev.preventDefault(); let newState = { key: Date.now() + this.props.user, text: this.state.currentText, user: this.props.user, counter: 0 }; if (newState.text !== '') { let item = [...this.state.items, newState]; this.setState({ currentState: newState, items: item, currentText: '' }) } else { alert('Wrong Input') } }
+    deleteAnswer = (key) => {
+        const filteredAnswers = this.state.answerItems.filter(item => { return item.key !== key });
+        this.setState({ answerItems: filteredAnswers })
+    }
+    addItem = (ev) => {
+        ev.preventDefault();
+        let newState = {
+            key: Date.now() + this.props.user,
+            text: this.state.currentText,
+            user: this.props.user,
+            counter: 0
+        };
+        if (newState.text !== '') {
+            let item = [...this.state.items, newState];
+            this.setState({ currentState: newState, items: item, currentText: '' })
+        }
+        else { alert('Wrong Input') }
+    }
+
     handleItem = (ev) => { this.setState({ currentText: ev.target.value }) }
-    addAnswer = (ev) => { ev.preventDefault(); let newState = { key: Date.now() + this.props.user, text: this.state.currentText, user: this.props.user, counter: 0 }; if (newState.text !== '') { let item = [...this.state.answerItems, newState]; this.setState({ currentAnswer: newState, answerItems: item, currentText: '' }) } else { alert('Wrong Input') } }
+    addAnswer = (ev) => {
+        ev.preventDefault();
+        let newState = {
+            key: Date.now() + this.props.user,
+            text: this.state.currentText,
+            user: this.props.user,
+            counter: 0
+        };
+        if (newState.text !== '') {
+            let item = [...this.state.answerItems, newState];
+            this.setState({ currentAnswer: newState, answerItems: item, currentText: '' })
+        }
+        else { alert('Wrong Input') }
+    }
     render() {
         return (
             <div className='todolist' >
@@ -71,8 +101,10 @@ class Todo extends Component {
                         <div id='Postbar'>
                             <TextArea id='Messaging' placeholder='Type something here' value={this.state.currentText} onChange={this.handleItem}></TextArea>
 
-                            <Button id='posttwo' size='large' type='primary' onClick={this.addItem}>Post</Button>
-                            <Button id='postthree' size='large' type='primary' onClick={this.addAnswer}>
+                            <Button id='posttwo' size='large' type='primary'
+                                onClick={this.addItem}>Post</Button>
+                            <Button id='postthree' size='large' type='primary'
+                                onClick={this.addAnswer}>
                                 <Icon type="star" theme="twoTone" />Add Answer
                         </Button>
                         </div>
