5400682ff00c6850b6ebc190f4a44270764479ec
Warren <warrenlowkh@gmail.com>
2020-01-18 00:47:17 +0800

    Updated Explore to newest version

--- client/src/Explore.js
+++ client/src/Explore.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Card, message, Carousel, Avatar, Icon, Spin, List, Divider, Button } from 'antd';
-import CarouselSlide from "./CauroselSlide"
+import { Card, message, Carousel, Avatar, Icon, Spin, List, Divider, Button, Input } from 'antd';
+import CarouselSlide from "./CauroselSlide.js";
+import ExploreTopBar from "./ExploreTopBar";
 import './index.css';
-
+import {Link} from 'react-router-dom'
 import InfiniteScroll from 'react-infinite-scroller';
 import ExploreCategories from './ExploreCategories';
-import { Link } from 'react-router-dom';
+
 const { Meta } = Card;
 
+const { Search } = Input;
 
-class Explore extends React.Component {
+
+export default class Explore extends React.Component {
 
     constructor(props) {
         super(props);
@@ -53,7 +56,6 @@ class Explore extends React.Component {
             this.setState({
                 hasMore: false,
                 loading: false,
-                container: { color: 'red', fontSize: '244%' }
             });
 
         }
@@ -73,8 +75,7 @@ class Explore extends React.Component {
                 msOverflowStyle: "none",
                 overflowX: "hidden",
             }}>
-
-                <div id="showcase" style={{ marginLeft: "1.3vw", marginBottom: "10vh" }}>
+                <div id="showcase" style={{ marginLeft: "1.3vw", marginBottom: "5vh" }}>
                     <Link to='/DiscApp'>
                         Hello(LINK TO DISCUSSION PAGE FOR DEBUGGING PURPOSES)
                     </Link>
@@ -89,59 +90,78 @@ class Explore extends React.Component {
                     }}>
                         Computing
                     </Link>
+                    <ExploreTopBar />
+
+                    <Divider orientation="left" style={{ color: "white", fontSize: "2vw" }}>
+                        <span>Spotlight </span>
+                        <Icon type="bulb"></Icon>
+                    </Divider>
+                    <Carousel dotPosition="right">
+                        <div>
+                            <CarouselSlide username="Tkai" url="https://www.youtube.com/watch?v=dATuq8O3920" viewers={156} desc="Join me in reviewing computing... an amazing subject everyone will love, I am serious about this. Totally not trying to extend the break jfjwefijnsdvkjgwdjneaghegherhjeghueguheguiauhaegeajgegjhiiiiiiiiljllllllllllllllllllllllllllllllllllifsafcasa38374732893129372173721382173394judfchiawebgvuqhwevyguhgvurhufgwqerugvhwqughuqhguqeghhwq3rgiyow3yuhgwuh" />
+                        </div>
+                        <div>
+                            <CarouselSlide username="William" url="https://www.youtube.com/watch?v=dATuq8O3920" viewers={156} desc="Join me in reviewing computing... an amazing subject everyone will love, I am serious about this. Totally not trying to extend the break jfjwefijnsdvkjgwdjneaghegherhjeghueguheguiauhaegeajgegjhiiiiiiiiljllllllllllllllllllllllllllllllllllifsafcasa38374732893129372173721382173394judfchiawebgvuqhwevyguhgvurhufgwqerugvhwqughuqhguqeghhwq3rgiyow3yuhgwuh" />
+                        </div>
+                        <div>
+                            <CarouselSlide username="Warren" url="https://www.youtube.com/watch?v=dATuq8O3920" viewers={156} desc="Join me in reviewing computing... an amazing subject everyone will love, I am serious about this. Totally not trying to extend the break jfjwefijnsdvkjgwdjneaghegherhjeghueguheguiauhaegeajgegjhiiiiiiiiljllllllllllllllllllllllllllllllllllifsafcasa38374732893129372173721382173394judfchiawebgvuqhwevyguhgvurhufgwqerugvhwqughuqhguqeghhwq3rgiyow3yuhgwuh" />
+                        </div>
+                        <div>
+                            <CarouselSlide username="Sherman" url="https://www.youtube.com/watch?v=dATuq8O3920" viewers={156} desc="Join me in reviewing computing... an amazing subject everyone will love, I am serious about this. Totally not trying to extend the break jfjwefijnsdvkjgwdjneaghegherhjeghueguheguiauhaegeajgegjhiiiiiiiiljllllllllllllllllllllllllllllllllllifsafcasa38374732893129372173721382173394judfchiawebgvuqhwevyguhgvurhufgwqerugvhwqughuqhguqeghhwq3rgiyow3yuhgwuh" />
+                        </div>
+                    </Carousel>
+                </div>
+
+                <Divider orientation="left" style={{ color: "white", fontSize: "2vw" }}>
+                    <span>Categories </span>
+                    <Icon type="unordered-list"></Icon>
+                </Divider>
+
+                <div id="Category">
+                    <ExploreCategories />
                 </div>
 
 
-                    <Divider orientation="left" style={{ color: "white", fontSize: "2vw" }}>
-                        <span>Categories </span>
-                        <Icon type="unordered-list"></Icon>
-                    </Divider>
+                <Divider orientation="left" style={{ color: "white", fontSize: "2vw" }}>
+                    <span>Hot Posts </span>
+                    <Icon type="fire" theme="twoTone" twoToneColor='red' />
+                </Divider>
 
-                    <div id="Category">
-                        <ExploreCategories />
-                    </div>
-
-
-                    <Divider orientation="left" style={{ color: "white", fontSize: "2vw" }}>
-                        <span>Hot Posts </span>
-                        <Icon type="fire" theme="twoTone" twoToneColor = 'red' />
-                    </Divider>
-
-                    <InfiniteScroll
-                        initialLoad={false}
-                        pageStart={0}
-                        loadMore={this.handleInfiniteOnLoad.bind(this)} //Function to handle infinite load
-                        hasMore={!this.state.loading && this.state.hasMore} //If [Not Loading] && [Has More Content], then true
-                        useWindow={false}
-                    >
-                        <List
-                            grid={{ gutter: 30, column: 3 }}
-                            dataSource={this.state.data}
-                            locale={{
-                                emptyText: (
-                                    <div className="demo-loading-container" style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
-                                        <Spin size="large" />
-                                    </div>
-                                )
-                            }}
-                            renderItem={item => (
-                                <List.Item key={item.id}>
-                                    <div onClick={this.cardClick} key={item.id}>
-                                        <Card
-                                            hoverable
-                                            type="inner"
-                                            bordered={false}
-                                            title="Post Title"
-                                            headStyle={{ backgroundColor: "#1890ff", color: "white", }}
-                                            bodyStyle={{ backgroundColor: "#001529" }}
-                                            style={{ boxShadow: "8px 0px 12px" }}
-                                            cover={<img alt="example" src={require('./assets/questionexample.jpeg')} />}
-                                        >
-                                            <Meta
-                                                title={
-                                                    <div id="Title" style={{ display: "flex", alignItems: "center", justifyItems: "center" }}>
-                                                        <Avatar style={{ backgroundColor: "#1890ff" }} size={45}>
-                                                            Tkai
+                <InfiniteScroll
+                    initialLoad={false}
+                    pageStart={0}
+                    loadMore={this.handleInfiniteOnLoad.bind(this)} //Function to handle infinite load
+                    hasMore={!this.state.loading && this.state.hasMore} //If [Not Loading] && [Has More Content], then true
+                    useWindow={false}
+                >
+                    <List
+                        grid={{ gutter: 30, column: 3 }}
+                        dataSource={this.state.data}
+                        locale={{
+                            emptyText: (
+                                <div className="demo-loading-container" style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+                                    <Spin size="large" />
+                                </div>
+                            )
+                        }}
+                        renderItem={item => (
+                            <List.Item key={item.id}>
+                                <div onClick={this.cardClick} key={item.id}>
+                                    <Card
+                                        hoverable
+                                        type="inner"
+                                        bordered={false}
+                                        title="Post Title"
+                                        headStyle={{ backgroundColor: "#1890ff", color: "white", }}
+                                        bodyStyle={{ backgroundColor: "#001529" }}
+                                        style={{ boxShadow: "8px 0px 12px" }}
+                                        cover={<img alt="example" src={require('./assets/questionexample.jpeg')} />}
+                                    >
+                                        <Meta
+                                            title={
+                                                <div id="Title" style={{ display: "flex", alignItems: "center", justifyItems: "center" }}>
+                                                    <Avatar style={{ backgroundColor: "#1890ff" }} size={45}>
+                                                        Tkai
                                                     </Avatar>
                                                     <h1 style={{ marginLeft: "1vw", color: "white", fontSize: "1.5vw" }}>{item.name.first}</h1>
                                                     <Button style={{ marginLeft: "auto", backgroundColor: "#fffb8f" }}>Mathematics</Button>
@@ -149,7 +169,7 @@ class Explore extends React.Component {
                                             }
                                             description={
                                                 <div id="Description">
-                                                    <p style={{ marginTop: "2vh", color: "white", fontSize: "1.3vw", fontWeight: "bold" }}>{this.props.viewers} Title: <Icon type="eye" theme="twoTone" twoToneColor="red" /></p>
+                                                    <p style={{ marginTop: "2vh", color: "white", fontSize: "1.3vw", fontWeight: "bold" }}>{this.props.viewers} Viewing Now <Icon type="eye" theme="twoTone" twoToneColor="red" /></p>
                                                     <p style={{ color: "white" }}>{item.name.title}</p>
                                                 </div>
                                             }
@@ -173,4 +193,4 @@ class Explore extends React.Component {
     }
 }
 
-export default Explore;
+
