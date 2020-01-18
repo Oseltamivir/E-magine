9584cbc1ffe672f733f7cdcfeca692241439eb16
Tkaixiang <tkaixiang@gmail.com>
2020-01-18 18:58:33 +0800

    To-do-list partial implementation of API

--- client/src/to-do-list.js
+++ client/src/to-do-list.js
@@ -10,7 +10,10 @@ const { TextArea } = Input
 class Todo extends Component {
     constructor(prop) {
         super(prop)
+
         this.state = {
+            data: this.props.data,
+            token: this.props.token,
             items: [],
             currentState: { key: '', text: '', user: '', counter: '' },
             currentText: '',
@@ -18,6 +21,10 @@ class Todo extends Component {
             answerItems: []
         }
 
+    }
+
+    fetchPostsData() {
+        
     }
     upvoteAnswer = (key) => {
         let ind = this.state.answerItems.findIndex(x => x.key === key);
@@ -95,8 +102,8 @@ class Todo extends Component {
                         <Icon type="question-circle" theme="twoTone" />
                     </Divider>
                     <p style={{ color: 'white' }}>Posted by {this.props.user} on {d.toDateString()}</p>
-                    <div dangerouslySetInnerHTML={{__html: this.props.title}}className = 'preview'></div>
-                    <div dangerouslySetInnerHTML={{ __html: this.props.post }} className='preview'></div>
+                    <div dangerouslySetInnerHTML={{__html: this.state.data.channel.title}}className = 'preview'></div>
+                    <div dangerouslySetInnerHTML={{ __html: this.state.data.channel.description }} className='preview'></div>
                     <Button type='primary' onClick={this.postOrEdit}><NavLink to = {{pathname:'/Posts_utils', state:{title:this.props.title, post:this.props.post}}}>Edit Post</NavLink></Button>
 
                     <span className ='votearea'>
