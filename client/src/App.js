fce30cb918efa77c47402c3fe366c95542311cae
willi123yao <willi123yao@gmail.com>
2020-01-19 12:07:04 +0800

    Pass messages and fetchMessageFromChannel to StreamDisc

--- client/src/App.js
+++ client/src/App.js
@@ -442,9 +442,9 @@ class App extends React.Component {
                   <Route exact path='/Explore/:topic' component={ExploreTopicPage} />
 
                   <Route exact path='/Profile' render={(props) => <Profile {...props} token={this.state.token} />} />
-                  <Route exact path='/DiscApp/:channel_id' component={(props) => <DiscApp {...props} messages={this.state.messages} fetchMessageFromChannel={this.fetchMessageFromChannel.bind(this)}/>} />
+                  <Route exact path='/DiscApp/:channel_id' component={(props) => <DiscApp {...props} messages={this.state.messages} fetchMessageFromChannel={this.fetchMessageFromChannel.bind(this)} />} />
 
-                  <Route exact path='/StreamsDiscussion/:channel_id' component={StreamDisc} />
+                  <Route exact path='/StreamsDiscussion/:channel_id' component={(props) => <StreamDisc {...props} messages={this.state.messages} fetchMessageFromChannel={this.fetchMessageFromChannel.bind(this)} />} />
                   <Route exact path='/CreatePost' render={(props) => <CreatePost {...props} token={this.state.token} />} />
                   <Route exact path='/DirectMessages' render={() =>
                     <DirectMsgs func={this.passInfo.bind(this)} />
